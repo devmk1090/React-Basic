aba2ab033610085e59d4bf6d7d367fcf3d25fc73
devmk1090 <devmk1090@gmail.com>
2024-01-26 01:40:27 +0900

    프로젝트명 변경 및 jsx 기본 문법 학습

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = { color : 'blue', fontSize : '30px' } 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div style={ posts }>REACT-BASIC</div>
+      </div>
     </div>
   );
 }
