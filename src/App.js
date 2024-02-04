dfca0f0244a74b709c6e693fd3a55e2b5ff939fc
devmk1090 <devmk1090@gmail.com>
2024-02-05 00:15:25 +0900

    React component - state (1)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = { color : 'blue', fontSize : '30px' } 
+  let [a, b] = useState([
+    'state는 변경되면 HTML이 자동 재렌더링 됨= HTML이 새로고침없어도 변경됨', 
+    '자주 바뀌는, 중요한 변수는 state에 넣자'
+  ]);
+
+  let posts = '강남 고기 맛집';  
   
   return (
     <div className="App">
       <div className='black-nav'>
-        <div style={ posts }>REACT-BASIC</div>
+        <div>React Basic</div>
+      </div>
+      <div className='list'>
+        <h3> { a[0] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { a[1] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
       </div>
     </div>
   );
