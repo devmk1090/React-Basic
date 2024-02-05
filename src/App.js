aad0fc9fc2d654805cc62e4804879151a25ba0e4
devmk1090 <devmk1090@gmail.com>
2024-02-06 01:40:02 +0900

    React component - state (2)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let [a, b] = useState([
+  let [title, setTitle] = useState([
     'state는 변경되면 HTML이 자동 재렌더링 됨= HTML이 새로고침없어도 변경됨', 
-    '자주 바뀌는, 중요한 변수는 state에 넣자'
+    '자주 바뀌는, 중요한 변수는 state에 넣자',
+    'state 변경함수로 변경해야 재렌더링이 잘 일어남'
   ]);
-
-  let posts = '강남 고기 맛집';  
+  let [like, setLike] = useState(0);
   
+
   return (
     <div className="App">
       <div className='black-nav'>
         <div>React Basic</div>
       </div>
       <div className='list'>
-        <h3> { a[0] } </h3>
-        <p>2월 17일 발행</p>
+        <h3> { title[0] } <span onClick={ ()=>{ setLike(like + 1) } }> 👍 </span> { like } </h3> 
+        <p>2월 1일 발행</p>
         <hr/>
       </div>
       <div className='list'>
-        <h3> { a[1] } </h3>
-        <p>2월 17일 발행</p>
+        <h3> { title[1] } </h3>
+        <p>2월 2일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { title[2] } </h3>
+        <p>2월 3일 발행</p>
         <hr/>
       </div>
     </div>
